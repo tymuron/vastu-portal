084874d5f5b3aed5f42c21022b66e1da3827a6ba
Ganna Chystiakova <annaromeo@Gannas-MacBook-Air.local>
2025-11-30 00:20:33 +0100

    Chore: Add production debug logging

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import App from './App.tsx'
 import './index.css'
 import { ErrorBoundary } from './components/ErrorBoundary'
 
+console.log('Main script executing...');
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ErrorBoundary>
